1a52ddf2e546419dad1c924ad4fa254c75f22f67
micro-machine <croteaumj@Michaels-MacBook-Pro.local>
2014-09-27 12:07:04 -0600

    change 'bitcore' refs to 'reddcore'

--- test/integration/blockExtractor.js
+++ test/integration/blockExtractor.js
@@ -8,8 +8,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var assert        = require('assert'),
   config          = require('../../config/config'),
   BlockExtractor  = require('../../lib/BlockExtractor'),
-  networks        = require('bitcore/networks'),
-  util            =  require('bitcore/util/util');
+  networks        = require('reddcore/networks'),
+  util            =  require('reddcore/util/util');
 
 var should = require('chai');
 //var txItemsValid = JSON.parse(fs.readFileSync('test/model/txitems.json'));
--- test/test.socket-server.js
+++ test/test.socket-server.js
@@ -6,7 +6,7 @@ var expect = chai.expect;
 var sinon = require('sinon');
 
 var socket = require('../app/controllers/socket');
-var bitcore = require('bitcore');
+var bitcore = require('reddcore');
 var EventEmitter = require('events').EventEmitter;
 
 describe('socket server', function() {
